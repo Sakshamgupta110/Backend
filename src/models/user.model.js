4e709477c638a6975843ea73b4db1205b1cc26fc
Sakshamgupta110 <sakshamgupta110@gmail.com>
2025-02-27 01:00:37 +0530

    register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
-    username:{
+    userName:{
         type:String,
         required:true,
         unique:true,
@@ -18,7 +18,7 @@ const userSchema = new Schema({
         trim:true,
         lowercase:true,
     },
-    fullname:{
+    fullName:{
         type:String,
         required:true,
         trim:true,
@@ -52,7 +52,7 @@ const userSchema = new Schema({
 
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 })
 
